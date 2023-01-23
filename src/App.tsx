5eae0d8de221ecef8894f5121f004d55c78ba47f
Natnael Mekonnen <it.natnael.mekonnen@gmail.com>
2023-01-23 19:44:10 +0300

    react + three.js starter kit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { AmbientLight } from 'three'
+
+function App() {
+	return <Canvas>{/* <AmbientLight></AmbientLight> */}</Canvas>
+}
+
+export default App
